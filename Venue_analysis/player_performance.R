601df732f12b59ed98d4d8b3ec40d7dc60a5a7ad
EscanorTheInvincible <abhilashpatade09@gmail.com>
2023-11-26 20:07:25 +0000

    notIMP

--- Venue_analysis/player_performance.R
+++ Venue_analysis/player_performance.R
@@ -4,38 +4,6 @@ library(plotly)
 
 # Read your CSV file
 cricket_data <- read.csv("ball_by_ball_it20.csv")
-# print(head(cricket_data))
-
-# print(colnames(cricket_data))
-# # Assuming 'Player_Name' is the player you want to track
-# player_name <- "V Kohli"
-
-# # Filter data for the specific player
-# player_data <- cricket_data %>%
-#   filter(Batter == player_name)
-
-# # Convert 'Date' to a date format
-# player_data$Date <- as.Date(player_data$Date)
-
-# # Create an interactive line plot
-# a <- plot_ly(
-#   data = player_data,
-#   x = ~Date,
-#   y = ~Total.Batter.Runs,
-#   type = "scatter",
-#   name = player_name,
-#   text = ~paste("Date: ", Date, "<br>Total Batter Runs: ", Total.Batter.Runs),
-#   hoverinfo = "text"
-# ) %>%
-#   layout(
-#     title = paste("Player's Performance Over the Years -", player_name),
-#     xaxis = list(title = "Date"),
-#     yaxis = list(title = "Total Batter Runs")
-#   )
-
-# print(a)
-
-
 
 bowler_data <- cricket_data %>%
   group_by(Bowler) %>%
